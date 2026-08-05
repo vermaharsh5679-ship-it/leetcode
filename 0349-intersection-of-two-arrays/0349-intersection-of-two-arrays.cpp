9d434fad7689d4fe4e4999699e9c91c1f2ef86a2
Harsh Verma <vermaharsh5679@gmail.com>
2026-08-05 13:50:01 +0530

    Time: 4 ms (22.09%), Space: 14.3 MB (47.16%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        std::sort(nums1.begin(), nums1.end());
-        std::sort(nums2.begin(), nums2.end());
-        std::vector<int> result;
-        int i = 0, j = 0;
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                if (result.empty() || result.back() != nums1[i]) {
-                    result.push_back(nums1[i]);
+        std::vector<int> lst;
+
+        // Nested loops to find matching elements
+        for (int i : nums1) {
+            for (int j : nums2) {
+                if (i == j) {
+                    lst.push_back(i);
                 }
-                i++;
-                j++;
             }
         }
-        return result;
+
+        // Convert vector to unordered_set to eliminate duplicates
+        std::unordered_set<int> set1(lst.begin(), lst.end());
+
+        // Convert set back to vector
+        std::vector<int> set2(set1.begin(), set1.end());
+
+        return set2;
     }
 };
