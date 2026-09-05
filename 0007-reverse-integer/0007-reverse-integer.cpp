1570951e2be0dbddf61f44e1c3c4419704baad0d
Harsh Verma <vermaharsh5679@gmail.com>
2026-09-05 21:13:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long revnum = 0;
+        long long revnum = 0; // using long long to fit the value
         while (x != 0) {
             int digit = x % 10;
-            revnum = (revnum * 10) + digit;
+            revnum = (revnum * 10) + digit; // simple while loop reversal technique
             x /= 10;
         }
        if(revnum>INT_MAX || revnum<INT_MIN){
-        return 0;
+        return 0;// checking for boundry case if number is greater than 32 bit signed return 0
        }
        return revnum;
     }
