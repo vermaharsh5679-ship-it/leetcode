5278019a9d286df34d46cf7183f1fab59cb779ea
Harsh Verma <vermaharsh5679@gmail.com>
2026-09-07 12:24:39 +0530

    Time: 0 ms (100%), Space: 10 MB (94.21%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    private:
+    int fuval(char c){
+         switch(c){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+    }
+    return 0;
+    }
+public:
+    int romanToInt(string s) {
+        int total=0;
+       for(int i=0;i<s.size()-1;i++){
+        int current =fuval(s[i]);
+        int next= fuval(s[i+1]);
+        if(current<next){
+            total-=current;
+        }
+        else{
+            total+=current;
+        }
+       }
+       total+=fuval(s[s.size()-1]);
+       return total;
+        }
+};
