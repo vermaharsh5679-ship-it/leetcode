f388f3f91d765d67f57768ed21565687b6484acf
Harsh Verma <vermaharsh5679@gmail.com>
2026-09-15 20:20:40 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.73%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open=0;
+        int close=0;
+        for(char c:s){
+            if(c=='('){
+                close++;
+            }
+            else{
+                if(close>0){
+                    close--;
+                }
+                else{
+                    open++;
+                }
+            }
+        }
+        return open+close;
+    }
+};
